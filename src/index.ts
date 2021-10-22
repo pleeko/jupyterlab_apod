6865426bdfab2ab3b448aae9d58b4488ef444be0
work <peterjfranchina@gmail.com>
2021-10-22 12:16:02 -0400

    Add styling, attribution, error handling

--- src/index.ts
+++ src/index.ts
@@ -27,14 +27,18 @@ const extension: JupyterFrontEndPlugin<void> = {
 
     // Create a blank content widget inside of a MainAreaWidget
     const content = new Widget();
+    content.addClass('my-apodWidget');
     const widget = new MainAreaWidget({ content });
     widget.id = 'apod-jupyterlab';
     widget.title.label = 'Astronomy Picture';
     widget.title.closable = true;
 
     // Add an image element to the content
-    let img = document.createElement('img');
+    const img = document.createElement('img');
     content.node.appendChild(img);
+
+    const summary = document.createElement('p');
+    content.node.appendChild(summary);
 
     // Get a random date string in YYYY-MM-DD format
     function randomDate() {
@@ -50,18 +54,31 @@ const extension: JupyterFrontEndPlugin<void> = {
     const response = await fetch(
       `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`
     );
-    const data = (await response.json()) as APODResponse;
-
-    if (data.media_type === 'image') {
-      // Populate the image
-      img.src = data.url;
-      img.title = data.title;
+    if (!response.ok) {
+      const data = await response.json();
+      if (data.error) {
+        summary.innerText = data.error.message;
+      } else {
+        summary.innerText = response.statusText;
+      }
     } else {
-      console.log('Random APOD was not a picture.');
+      const data = (await response.json()) as APODResponse;
+
+      if (data.media_type === 'image') {
+        // Populate the image
+        img.src = data.url;
+        img.title = data.title;
+        summary.innerText = data.title;
+        if (data.copyright) {
+          summary.innerText += ` (Copyright ${data.copyright})`;
+        }
+      } else {
+        summary.innerText = 'Random APOD fetched was not an image.';
+      }
     }
 
     // Add an application command
-    const command: string = 'apod:open';
+    const command = 'apod:open';
     app.commands.addCommand(command, {
       label: 'Random Astronomy Picture',
       execute: () => {
